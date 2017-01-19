13340e886c60dd775c6c920a367552c6a4f646e2
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-01-18 21:30:25 -0800

    improvements

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const http = require("http"),
-      request = require("request");
+      fs = require("fs"),
+      request = require("request"),
+      mustache = require("mustache")
 
 const STORY_ENDPOINT = "https://fimfiction.net/api/story.php?story=";
+const DOWNLOAD_ENDPOINT = "https://fimfiction.net/download_story.php?story=";
+
+const styles = fs.readFileSync("style.css");
+
+const templates = {
+    story: fs.readFileSync("story.html").toString(),
+    home: fs.readFileSync("home.html").toString()
+}
 
 http.createServer( (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
 
     if(req.url == "/")  {
-        res.end(home());
+        res.end(mustache.render(templates.home, {style: styles}));
     } else {
         const parts = req.url.split("/")
         const verb = parts[1];
@@ -21,13 +31,19 @@ http.createServer( (req, res) => {
                try {
                    const story = JSON.parse(body).story;
 
-                   console.log(story);
-           
-                   res.end(story.title);
+                   story.style = styles;
+
+                   res.end(mustache.render(templates.story, story));
                } catch(e) {
                    console.log("Bad body " + body);
                }
            });
+        } else if(verb == "read") {
+            /* fetch the story via the download HTML button (messy but meh) */
+
+            request(DOWNLOAD_ENDPOINT + object + "&html", (err, resp, body) => {
+                res.end(body + "<style>" + styles + "</style>");
+            });
         } else {
             res.end("Unknown verb " + verb);
         }
