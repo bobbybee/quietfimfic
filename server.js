42fd2b06371e0a11d2e4b47950ceff778f0c8b37
Alyssa Rosenzweig <alyssa@rosenzweig.io>
2017-01-21 10:22:35 -0800

    Misc cleanup

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+"use strict";
+
 const http = require("http"),
       fs = require("fs"),
       request = require("request"),
@@ -15,14 +17,14 @@ const templates = {
     home: fs.readFileSync("home.html").toString()
 };
 
-errcb = (err) => err ? console.error(err) : null;
-erras = (err, v) => err ? console.error(err) : v;
-srequest = (u, cb) => request(u, (e, r, b) => erras(e, cb(b)));
+const errcb = (err) => err ? console.error(err) : null;
+const erras = (err, v) => err ? console.error(err) : v;
+const srequest = (u, cb) => request(u, (e, r, b) => erras(e, cb(b)));
 
-cache = (endpoint, id) => "cache/" + endpoint + "/" + id;
-fetch = (ep, id, cb) => fs.readFile(cache(ep, id), (e, data) =>
-    e ? srequest(endpoints[ep](id), (r) => (fs.writeFile(cache(ep, id), r, errcb), cb(r)))
-    :    cb(data));
+const cache = (endpoint, id) => "cache/" + endpoint + "/" + id;
+const fetch = (ep, id, cb) => fs.readFile(cache(ep, id), (e, data) =>
+                e ? srequest(endpoints[ep](id), (r) => (fs.writeFile(cache(ep, id), r, errcb), cb(r)))
+                :   cb(data));
 
 const verbs = {
     "read": (id, cb) => fetch("read", id, (body) => {
@@ -30,7 +32,6 @@ const verbs = {
     }),
 
     "story": (id, cb) => fetch("story", id, (body) => {
-        console.log(body);
         const story = JSON.parse(body).story;
         story.style = styles;
 
@@ -41,17 +42,21 @@ const verbs = {
 http.createServer( (req, res) => {
     res.writeHead(200, {"Content-Type": "text/html"});
 
-    if(req.url == "/")  {
-        res.end(mustache.render(templates.home, {style: styles}));
-    } else {
-        const parts = req.url.split("/")
-        const verb = parts[1];
-
-        if(typeof verbs[verb] !== 'undefined') {
-            verbs[verb](parts[2], (a) => res.end(a));
+    try {
+        if(req.url == "/")  {
+            res.end(mustache.render(templates.home, {style: styles}));
         } else {
-            res.end("Unknown verb " + verb);
+            const parts = req.url.split("/");
+            const verb = parts[1];
+
+            if(typeof verbs[verb] !== 'undefined') {
+                verbs[verb](parts[2], (a) => res.end(a));
+            } else {
+                res.end("Unknown verb " + verb);
+            }
         }
+    } catch(e) {
+        console.error(e);
     }
 
 }).listen(8080);
